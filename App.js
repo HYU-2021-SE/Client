2a8d6c6f912c0490c16b4289a8259667f3144228
yerincho <forjustice9839@gmail.com>
2021-11-10 13:15:15 +0900

    feat: set global font

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import Styled from 'styled-components/native';
 import { Text } from 'react-native';
+import { setCustomText } from 'react-native-global-props';
 
 const App = () => {
+  setCustomText(customTextProps);
   return (
     <Container>
-      <Text>시작 페이지임</Text>
+      <Text>calendar</Text>
+      <Text>한글</Text>
     </Container>
   );
 };
 
-const Container = Styled.SafeAreaView`
+const customTextProps = {
+  style: {
+    fontFamily: 'MaruBuri',
+  },
+};
+
+const Container = Styled.View`
   flex: 1;
   background-color: #fff;
   align-items: center;
