7fe49159e57363145420a81feb9ad7ccb9966bf6
yerincho <forjustice9839@gmail.com>
2021-12-09 23:51:08 +0900

    refactor: elastic ip

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,4 @@
-export const apiRequestUrl = 'http://3.15.158.154:8080/api';
-// export const apiRequestUrl = 'http://192.168.0.4:8080/api';
+export const apiRequestUrl = 'http://18.222.61.22:8080/api';
 
 export const userAgent =
   'Mozilla/5.0 (iPhone; CPU iPhone OS 15_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1';
